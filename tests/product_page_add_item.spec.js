137bce3f304d3063683ac325e753da90bc7d5803
juuanek <johnus1122@wp.pl>
2025-03-17 23:03:59 +0100

    First test in playwright

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -2,6 +2,10 @@ import { test } from "@playwright/test"
 
 test("Product Page Add To Basket", async ({ page }) => {
     await page.goto("localhost:2221")
-    await page.pause()
+    // await page.pause()
+
+    const addToBasketButton = page.getByRole('button', { name: 'Add to Basket' }).first()
+    await addToBasketButton.waitFor()
+    await addToBasketButton.click()
 
 })
