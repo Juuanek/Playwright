85c58bc273ff6098ca9fd15c03386ff231d0eab4
juuanek <johnus1122@wp.pl>
2025-04-18 22:51:57 +0200

    Fisnished 13th. Can start 14th now

--- tests/full_e2e.spec.js
+++ tests/full_e2e.spec.js
@@ -5,7 +5,10 @@ test.only("New user full e2e test journey", async ({ page }) => {
 
     const productsPage = new ProductsPage(page)
     await productsPage.visit()
+    await productsPage.addProductToBasket(0)
+    await productsPage.addProductToBasket(1)
+    await productsPage.addProductToBasket(2)
+
     await page.pause()
-    await productsPage.visit() // to be deleted
     
 })
