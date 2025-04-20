6ef4f9845dd9b83839f87604577f02d7f9dc5f73
juuanek <johnus1122@wp.pl>
2025-04-20 14:43:49 +0200

    Finished 15th. Can start 16th now

--- page-objects/ProductsPage.js
+++ page-objects/ProductsPage.js
@@ -4,19 +4,32 @@ export class ProductsPage {
     constructor(page) {
         this.page = page;
         this.addButtons = page.locator('[data-qa="product-button"]')
+        this.basketCounter = page.locator('[data-qa="header-basket-count"]')
     }
  
     visit = async () => {
         await this.page.goto("/")
     }
 
+    getBasketCount = async () => {
+        await this.basketCounter.waitFor()
+        //return a number
+        const text = await this.basketCounter.innerText()
+        // "0" -> 0
+        const asNumber = parseInt(text, 10)
+        return asNumber
+        } 
+
     addProductToBasket = async (index) => {
         const specificButton = this.addButtons.nth(index)
         
         await specificButton.waitFor()
         await expect (specificButton).toHaveText("Add to Basket")
+        const basketCountBeforeAdding = await this.getBasketCount()
         await specificButton.click()
         await expect (specificButton).toHaveText("Remove from Basket")
+        const basketCountAfterAdding = await this.getBasketCount()
+        expect (basketCountAfterAdding).toBeGreaterThan(basketCountBeforeAdding)
 
     }
 }
--- tests/full_e2e.spec.js
+++ tests/full_e2e.spec.js
@@ -8,6 +8,7 @@ test.only("New user full e2e test journey", async ({ page }) => {
     await productsPage.addProductToBasket(0)
     await productsPage.addProductToBasket(1)
     await productsPage.addProductToBasket(2)
+    
 
     // await page.pause()
     
