5de56a76ef9fc1bb3ad52c9c8efa6c11f573be51
juuanek <johnus1122@wp.pl>
2025-06-07 22:11:37 +0200

    Finished 31

--- page-objects/PaymentPage.js
+++ page-objects/PaymentPage.js
@@ -1,13 +1,17 @@
+import { expect } from "@playwright/test"
 export class PaymentPage {
     constructor(page) {
         this.page = page
         this.discountCode = page.frameLocator('[data-qa="active-discount-container"]')
                                 .locator('[data-qa="discount-code"]')
-
+        this.discountCodeInput = page.locator('[data-qa="discount-code-input"]')
     }
     activateDiscount = async () => {
         await this.discountCode.waitFor()
-        console.log(this.discountCode)
-        await this.page.pause()
+        const code = await this.discountCode.innerText()
+        console.log(code)
+        await this.discountCodeInput.waitFor()
+        await this.discountCodeInput.fill(code)
+        await expect(this.discountCodeInput).toHaveValue(code)
     }
 }
